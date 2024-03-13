5142feec656d38d75ebdd3de1959736680e127a4
lukaszruman <161606263+lukaszruman@users.noreply.github.com>
2024-03-13 13:21:24 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    String message = "Hello World";
+    for (char c : message.toCharArray()) {
+      System.out.println(c);
+    }
+  }
+}
